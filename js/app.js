52ab965e742345014a9fb46747cfd633a29d4ec1
Romain32190 <romain.lafargue36@gmail.com>
2017-03-15 21:57:08 +0100

    Mise à jour

--- js/app.js
+++ js/app.js
@@ -1,29 +1,29 @@
-addition = function(a, b) {
-console.log(a + b);
+	addition = function(a, b) {
+	console.log(a + b);
 
-};
- 
-substraction = function(a, b) {
-console.log(a - b);
-};
+	};
+	 
+	substraction = function(a, b) {
+	console.log(a - b);
+	};
 
-multiplication = function(a, b) {
-console.log(a * b);
+	multiplication = function(a, b) {
+	console.log(a * b);
 
-};
+	};
 
-division = function(a, b) {
-	if(b > 0){
-		console.log(a / b);
-	} else {
-		console.log("Can't divide by 0");
-	}
+	division = function(a, b) {
+		if(b > 0){
+			console.log(a / b);
+		} else {
+			console.log("Can't divide by 0");
+		}
 
 
-};
+	};
 
-addition(6, 5);
-substraction(5, 4);
-multiplication(6, 4);
-division(20, 2);
+	addition(6, 5);
+	substraction(5, 4);
+	multiplication(6, 4);
+	division(20, 2);
 
